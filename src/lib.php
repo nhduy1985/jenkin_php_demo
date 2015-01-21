9df9f738c85d5df62f528e2f9a17afee4de761aa
Duy Tran <duy.tran@bluecomgroup.com>
2015-01-21 17:21:04 +0700

    Add another method for testing

--- src/lib.php
+++ src/lib.php
@@ -4,4 +4,8 @@ class Lib {
 	function add($param1,$param2) {
 		return $param1 - $param2;
 	}
+
+	function substract($param1,$param2) {
+		return $param1 - $param2;
+	}
 }
--- tests/libTest.php
+++ tests/libTest.php
@@ -6,5 +6,11 @@ class LibTest extends PHPUnit_Framework_TestCase
         $lib = new Lib();
         $this->assertEquals(2, $lib->add(1,1));
     }
+
+    public function testSubtract()
+    {
+        $lib = new Lib();
+        $this->assertEquals(0, $lib->subtract(1,1));
+    }
 }
 ?>
