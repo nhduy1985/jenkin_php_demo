d691067538a99df0de4ac2674600dac978fd9c51
Duy Tran <duy.tran@bluecomgroup.com>
2015-01-21 17:26:17 +0700

    Rename

---
+++ src/Lib.php
@@ -0,0 +1,15 @@
+<?php
+
+class Lib {
+	function add($param1,$param2) {
+		return $param1 - $param2;
+	}
+
+	function substract($param1,$param2) {
+		return $param1 - $param2;
+	}
+
+	function multiple($param1,$param2) {
+		return $param1 * $param2;
+	}
+}
